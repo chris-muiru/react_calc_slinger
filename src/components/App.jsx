9c72c7364105671b014fbaae839921e831c2eac0
kris@git <anonymousguy1000000@gmail.com>
2021-09-01 11:42:31 -0400

    feat: the App component

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+import Display from './Display';
+import Keypad from './Keypad';
+import '../css/app.css';
+
+
+const App = (props) => {
+    const [keyValue, setValue] = useState('');
+    return ( 
+        <div className="app">
+            <Display keyValue={keyValue} setValue={setValue}/>
+            <Keypad keyValue={keyValue} setValue={setValue} id={id}/>
+        </div>
+     );
+}
+ 
+export default App;
