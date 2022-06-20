9704cb7abd89c0a69ab91bbcd4b5d7000ade280e
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-20 11:42:15 +0300

    add: add key tha represent individual button

--- src/components/Key.jsx
+++ src/components/Key.jsx
@@ -1,16 +1,21 @@
-import '../css/key.css';
+import "../css/key.css";
 
-const  Key = ({ color, expand=false,keyValue,setValue,children,id}) => {
-    // a state
-    let style = { color };
-    const handleClick=()=>{setValue(keyValue+children)};
-    
-    const classList = `calc-key ${ expand ? "expanded": ""}`;
-    
-    return ( 
-            <button className={classList} style={style} id="btn-key" onClick={handleClick}>
-                {children}
-            </button>
-           );
-}
-export default Key ;
+const Key = ({ color, expand = false, keyValue, setValue, children, id }) => {
+	const handleClick = () => {
+		setValue(keyValue + children);
+	};
+
+	const classList = `calc-key`;
+	return (
+		<button
+			className={classList}
+			style={{ color }}
+			id="btn-key"
+			onClick={handleClick}
+		>
+			{children}
+		</button>
+	);
+};
+
+export default Key;
