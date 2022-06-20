9597cd709796e406171f4896fe3c2ce7ab23bc36
Chris Muiru <krisdjangodeveloper@gmail.com>
2022-06-20 11:42:52 +0300

    add: keypad that has all the keys

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -1,33 +1,85 @@
-import Key from './Key';
-import '../css/keypad.css';
-const Keypad  = ({keyValue,setValue,id,setId}) => {
-    const calculations=(id)=>{
-        
-    };
+import Key from "./Key";
+import "../css/keypad.css";
+import "../css/key.css";
+import { evaluate } from "mathjs";
+const classList = `calc-key`;
 
-    return ( 
-        <div className="keypad">
-                <Key color="#507d84" keyValue={keyValue} setValue={setValue}>C</Key> 
-                <Key color="#507d84" keyValue={keyValue} setValue={setValue}>+/-</Key> 
-                <Key color="#507d84" keyValue={keyValue} setValue={setValue}>%</Key> 
-                <Key color="#ea736e" keyValue={keyValue} setValue={setValue}>&divide;</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>7</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>8</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>9</Key> 
-                <Key color="#ea736e" keyValue={keyValue} setValue={setValue}>x</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue} >4</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue} >5</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue} >6</Key> 
-                <Key color="#ea736e" keyValue={keyValue} setValue={setValue}>-</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>1</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>2</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>3</Key> 
-                <Key color="#ea736e" keyValue={keyValue} setValue={setValue}  onclick={calculations()}>+</Key> 
-                <Key color="black" expand={true} keyValue={keyValue} setValue={setValue}>0</Key> 
-                <Key color="black" keyValue={keyValue} setValue={setValue}>.</Key> 
-                <Key color="#ea736e" keyValue={keyValue} setValue={setValue}>=</Key> 
-        </div>
-     );
-}
- 
-export default Keypad ;
+const Keypad = ({ keyValue, setValue }) => {
+	//calculation function
+
+	const calculations = () => {
+		let answer = evaluate(keyValue);
+		console.log(answer);
+		setValue(answer);
+		return answer;
+	};
+
+	return (
+		<div className="keypad">
+			<button color="#507d84" class={classList} onClick={() => setValue("")}>
+				C
+			</button>
+			<Key color="#507d84" keyValue={keyValue} setValue={setValue}>
+				+/-
+			</Key>
+			<Key color="#507d84" keyValue={keyValue} setValue={setValue}>
+				%
+			</Key>
+			<Key color="#ea736e" keyValue={keyValue} setValue={setValue}>
+				&divide;
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				7
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				8
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				9
+			</Key>
+			<Key color="#ea736e" keyValue={keyValue} setValue={setValue}>
+				*
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				4
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				5
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				6
+			</Key>
+			<Key color="#ea736e" keyValue={keyValue} setValue={setValue}>
+				-
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				1
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				2
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				3
+			</Key>
+			<Key color="#ea736e" keyValue={keyValue} setValue={setValue}>
+				+
+			</Key>
+			<Key
+				color="black"
+				expand={true}
+				keyValue={keyValue}
+				setValue={setValue}
+			>
+				0
+			</Key>
+			<Key color="black" keyValue={keyValue} setValue={setValue}>
+				.
+			</Key>
+			<button color="#ea736e" class={classList} onClick={() => calculations()}>
+				=
+			</button>
+		</div>
+	);
+};
+
+export default Keypad;
